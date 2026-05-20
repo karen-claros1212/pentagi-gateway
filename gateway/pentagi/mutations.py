907d7c92139a704f1cf6d7e30866f8952384f9df
karen claros <diegoclaros1983@gmail.com>
2026-05-20 17:13:24 -0500

    fix pentagi mutation return shapes

--- gateway/pentagi/mutations.py
+++ gateway/pentagi/mutations.py
@@ -4,51 +4,48 @@ Policy/approval checks must happen before these are called.
 These constants are schema-aligned for PentAGI v2.0.0 and are covered by mocked tests only.
 """
 
-RESULT_FIELDS = """
-    ok
-    message
-    error
+CREATE_FLOW_MUTATION = """
+mutation CreateFlow($modelProvider: String!, $input: String!) {
+  createFlow(modelProvider: $modelProvider, input: $input) {
+    id
+    title
+    status
+    provider {
+      name
+      type
+    }
+    createdAt
+    updatedAt
+  }
+}
 """
 
-CREATE_FLOW_MUTATION = f"""
-mutation CreateFlow($modelProvider: String!, $input: String!) {{
-  createFlow(modelProvider: $modelProvider, input: $input) {{
-{RESULT_FIELDS}
-  }}
-}}
+PUT_USER_INPUT_MUTATION = """
+mutation PutUserInput($flowId: ID!, $input: String!, $modelProvider: String) {
+  putUserInput(flowId: $flowId, input: $input, modelProvider: $modelProvider)
+}
 """
-PUT_USER_INPUT_MUTATION = f"""
-mutation PutUserInput($flowId: ID!, $input: String!) {{
-  putUserInput(flowId: $flowId, input: $input) {{
-{RESULT_FIELDS}
-  }}
-}}
+
+STOP_FLOW_MUTATION = """
+mutation StopFlow($flowId: ID!) {
+  stopFlow(flowId: $flowId)
+}
 """
-STOP_FLOW_MUTATION = f"""
-mutation StopFlow($flowId: ID!) {{
-  stopFlow(flowId: $flowId) {{
-{RESULT_FIELDS}
-  }}
-}}
+
+FINISH_FLOW_MUTATION = """
+mutation FinishFlow($flowId: ID!) {
+  finishFlow(flowId: $flowId)
+}
 """
-FINISH_FLOW_MUTATION = f"""
-mutation FinishFlow($flowId: ID!) {{
-  finishFlow(flowId: $flowId) {{
-{RESULT_FIELDS}
-  }}
-}}
+
+RENAME_FLOW_MUTATION = """
+mutation RenameFlow($flowId: ID!, $title: String!) {
+  renameFlow(flowId: $flowId, title: $title)
+}
 """
-RENAME_FLOW_MUTATION = f"""
-mutation RenameFlow($flowId: ID!, $title: String!) {{
-  renameFlow(flowId: $flowId, title: $title) {{
-{RESULT_FIELDS}
-  }}
-}}
-"""
-DELETE_FLOW_MUTATION = f"""
-mutation DeleteFlow($flowId: ID!) {{
-  deleteFlow(flowId: $flowId) {{
-{RESULT_FIELDS}
-  }}
-}}
+
+DELETE_FLOW_MUTATION = """
+mutation DeleteFlow($flowId: ID!) {
+  deleteFlow(flowId: $flowId)
+}
 """
